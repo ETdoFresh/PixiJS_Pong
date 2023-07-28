6ec104616d45ab6cf9d8af4e796dd20324652809
ETdoFresh <ETdoFresh@gmail.com>
2023-07-27 22:28:34 -0500

    Expand the canvas to fit the window. Add pointer support.

--- src/systems/ListenForInput.js
+++ src/systems/ListenForInput.js
@@ -10,10 +10,21 @@ export class ListenForInput extends System {
         const input = entity.getComponent(Components.Input);
         if (!input) return;
         input.keys = {};
+        input.quadrant = {}
         input.onKeyDown = (event) => { input.keys[event.key] = true; };
         input.onKeyUp = (event) => { input.keys[event.key] = false; };
+        input.onPointerDown = (event) => {
+            input.quadrant.x = event.clientX < window.innerWidth / 2 ? -1 : 1;
+            input.quadrant.y = event.clientY < window.innerHeight / 2 ? -1 : 1;
+            input.quadrant.pointerDown = true;
+        };
+        input.onPointerUp = (event) => {
+            input.quadrant.pointerDown = false;
+        };
         window.addEventListener('keydown', input.onKeyDown);
         window.addEventListener('keyup', input.onKeyUp);
+        window.addEventListener('pointerdown', input.onPointerDown);
+        window.addEventListener('pointerup', input.onPointerUp);
     }
 
     entityRemoved(entity) {
@@ -21,5 +32,7 @@ export class ListenForInput extends System {
         if (!input) return;
         window.removeEventListener('keydown', input.onKeyDown);
         window.removeEventListener('keyup', input.onKeyUp);
+        window.removeEventListener('pointerdown', input.onPointerDown);
+        window.removeEventListener('pointerup', input.onPointerUp);
     }
 }
--- src/systems/MovePaddleOnInput.js
+++ src/systems/MovePaddleOnInput.js
@@ -12,9 +12,13 @@ export class MovePaddleOnPlayer1Input extends System {
             const entity = entities[i];
             const input = entity.getComponent(Components.Input);
             const paddle = entity.getComponent(Components.Paddle);
-            if (input.keys['w']) {
+            const isKeyboardUpPressed = input.keys['w'];
+            const isKeyboardDownPressed = input.keys['s'];
+            const isPointerUpPressed = input.quadrant.pointerDown && input.quadrant.x === -1 && input.quadrant.y === -1;
+            const isPointerDownPressed = input.quadrant.pointerDown && input.quadrant.x === -1 && input.quadrant.y === 1;
+            if (isKeyboardUpPressed || isPointerUpPressed) {
                 paddle.velocity.y = -paddle.speed;
-            } else if (input.keys['s']) {
+            } else if (isKeyboardDownPressed || isPointerDownPressed) {
                 paddle.velocity.y = paddle.speed;
             } else {
                 paddle.velocity.y = 0;
@@ -34,9 +38,13 @@ export class MovePaddleOnPlayer2Input extends System {
             const entity = entities[i];
             const input = entity.getComponent(Components.Input);
             const paddle = entity.getComponent(Components.Paddle);
-            if (input.keys['ArrowUp']) {
+            const isKeyboardUpPressed = input.keys['ArrowUp'];
+            const isKeyboardDownPressed = input.keys['ArrowDown'];
+            const isPointerUpPressed = input.quadrant.pointerDown && input.quadrant.x === 1 && input.quadrant.y === -1;
+            const isPointerDownPressed = input.quadrant.pointerDown && input.quadrant.x === 1 && input.quadrant.y === 1;
+            if (isKeyboardUpPressed || isPointerUpPressed) {
                 paddle.velocity.y = -paddle.speed;
-            } else if (input.keys['ArrowDown']) {
+            } else if (isKeyboardDownPressed || isPointerDownPressed) {
                 paddle.velocity.y = paddle.speed;
             } else {
                 paddle.velocity.y = 0;
